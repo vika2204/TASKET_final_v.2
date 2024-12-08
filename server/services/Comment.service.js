268b9662a5e4dcef50feca045898087d03728fb0
Mukhammad <jonbola1999@gmail.com>
2024-12-08 17:37:22 +0300

    fix name of createrComment

--- server/services/Comment.service.js
+++ server/services/Comment.service.js
@@ -1,16 +1,17 @@
-const {Comment} = require('../db/models');
-const User = require('../db/models/User');
-class CommentService {
+const { Comment, User } = require("../db/models");
 
+class CommentService {
   static async getAllComments(ticket_id) {
     try {
       const comments = await Comment.findAll({
         where: { ticket_id: ticket_id },
-        include: [{model: User, as: 'user'}]
+        include: [{ model: User, as: "user" }],
       });
       return comments;
     } catch (error) {
-      throw new Error(`Error fetching comments for ticket_id: ${ticket_id}. ${error.message}`);
+      throw new Error(
+        `Error fetching comments for ticket_id: ${ticket_id}. ${error.message}`
+      );
     }
   }
 
@@ -23,29 +24,20 @@ class CommentService {
     }
   }
 
-
-
-
   static async addComment(data) {
-
-    
     try {
       const comment = await Comment.create(data);
       console.log(data);
-      
+
       return comment;
     } catch (error) {
       throw new Error(error.message);
     }
   }
 
- 
-
-  static async updateComment(data,id) {
+  static async updateComment(data, id) {
     try {
-
-      
-      const [comment] = await Comment.update(data, { where: { id }  });
+      const [comment] = await Comment.update(data, { where: { id } });
 
       return comment;
     } catch (error) {
